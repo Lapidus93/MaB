8e87c50df4d44e0c2e971c2ee44aff1b8df24879
Lapidus93 <47421699+Lapidus93@users.noreply.github.com>
2020-02-07 18:26:13 +0300

    1_push
    создание колод сделано

---
+++ code.py
@@ -0,0 +1,52 @@
+import random
+
+# КЛАСС КОЛОД
+class deck(object):
+    
+    def __init__(self, name, cards_id):
+        self.name = name
+        self.cards_id = cards_id
+   
+    def count_cards(self):
+        return len(self.cards_id)
+    
+    def take_card(self):
+        return random.choice(self.cards_id)
+
+    
+# КЛАСС КАРТ
+
+class card(object):
+    
+    def __init__(self, cid, name, description,actions, deck):
+        self.cid = cid
+        self.name = name
+        self.description = description
+        self.actions = actions
+         
+    def show_decks(self):
+        return 
+
+    def assign_card(self):
+        return 
+    
+    def show_card(self):
+        return 
+
+
+### СОЗДАНИЕ КОЛОД
+
+file = open('баланс_колод.txt', 'r', encoding="utf-8")
+list_of_strings = []
+
+x = file.readlines()
+for i in x:
+    y = i.split(';')
+    list_of_strings.append(y)
+    
+list_of_decks = []
+for i in list_of_strings:
+    dec = deck(i[0],i[1:])
+    list_of_decks.append(dec)
+    
+file.close()
